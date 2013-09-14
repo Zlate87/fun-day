d6e3b9fd65d9a324bcf2e5fdf5af51b5a07c74f0
oliverlaz <oliver.lazoroski@gmail.com>
2013-09-14 17:05:05 +0200

    added web client utility which can handle WebClient exceptions

---
+++ mkdict-parser/src/main/java/com/netcetera/codecamp/parser/WebClientUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 by Netcetera AG.
+ * All rights reserved.
+ *
+ * The copyright to the computer program(s) herein is the property of Netcetera AG, Switzerland.
+ * The program(s) may be used and/or copied only with the written permission of Netcetera AG or
+ * in accordance with the terms and conditions stipulated in the agreement/contract under which 
+ * the program(s) have been supplied.
+ */
+package com.netcetera.codecamp.parser;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+
+public final class WebClientUtils {
+  
+  @SuppressWarnings("unchecked")
+  public static <T extends HtmlElement> List<T> getByXPath(final String url, final String selector) {
+    final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_17);
+    try {
+      final HtmlPage page = webClient.getPage(new URL(url));
+      return (List<T>) page.getByXPath(selector);
+    } catch (Exception e) {
+      return Collections.emptyList();
+    }
+  }
+
+}
