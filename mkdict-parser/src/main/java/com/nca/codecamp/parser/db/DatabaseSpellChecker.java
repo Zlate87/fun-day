500fbb1c04f16e78e3408ff086bd7b8f1c0c065a
oliverlaz <oliver.lazoroski@gmail.com>
2013-09-15 02:46:01 +0200

    spell checker

---
+++ mkdict-parser/src/main/java/com/nca/codecamp/parser/db/DatabaseSpellChecker.java
@@ -0,0 +1,59 @@
+package com.nca.codecamp.parser.db;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+
+public class DatabaseSpellChecker {
+
+  public static void main(String[] args) throws IOException {
+    DatabaseUtilities.initializeJdbcDriver();
+    doSpellCheck();
+  }
+
+  private static void doSpellCheck() throws IOException {
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection(DatabaseContext.CONNECTION_STRING);
+      connection.setAutoCommit(false);
+
+      final List<String> lines = FileUtils.readLines(new File("mk_MK.dic"), Charset.forName("UTF-8"));
+
+      final Statement statement = connection.createStatement();
+      statement.executeUpdate("drop table if exists spellcheck ");
+      statement.executeUpdate("create table if not exists spellcheck (word text)");
+
+      final PreparedStatement preparedStatement = connection.prepareStatement("insert into spellcheck values (?)");
+      for (String line : lines) {
+        preparedStatement.setString(1, line);
+        preparedStatement.addBatch();
+      }
+
+      preparedStatement.executeBatch();
+
+      statement.executeUpdate("drop table if exists final_words");
+      statement.executeUpdate("create table if not exists final_words (word text, occurences number)");
+
+      statement.executeUpdate("insert into final_words "
+          + "select wo.word, wo.occurence as occurences from word_occurence wo "
+          + "where wo.word in (select * from spellcheck)");
+
+
+      connection.commit();
+    } catch (SQLException e) {
+      e.printStackTrace(System.err);
+    } finally {
+      DatabaseUtilities.closeConnection(connection);
+    }
+  }
+
+}
